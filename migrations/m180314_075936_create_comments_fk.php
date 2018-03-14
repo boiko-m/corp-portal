af8387727183b6697ca67b83e589442683b6323c
Лешик Максим Игоревич <leshik@lbr.ru>
2018-03-14 11:23:49 +0300

    created comments model

---
+++ migrations/m180314_075936_create_comments_fk.php
@@ -0,0 +1,68 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180314_075936_create_comments_fk
+ */
+class m180314_075936_create_comments_fk extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createIndex(
+            'idx-corplbr_comments-id_user',
+            'corplbr.comments',
+            'id_user'
+        );
+
+        $this->addForeignKey(
+            'fk-corplbr_comments-id_user',
+            'corplbr.comments',
+            'id_user',
+            'corplbr.[user]',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->createIndex(
+            'idx-corplbr_comments-id_user',
+            'corplbr.comments',
+            'id_user'
+        );
+
+        $this->addForeignKey(
+            'fk-corplbr_comments-id_user',
+            'corplbr.comments',
+            'id_user',
+            'corplbr.[user]',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180314_075936_create_comments_fk cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ models/Comments.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "corplbr.comments".
+ *
+ * @property int $id
+ * @property int $id_user
+ * @property int $fk_id
+ * @property string $type_fk
+ * @property string $title
+ * @property string $content
+ * @property string $dates
+ * @property int $visible
+ * @property int $admin_accept
+ * @property int $id_comment_to
+ */
+class Comments extends ActiveRecord
+{
+    const VISIBLE_INACTIVE = 0;
+    const VISIBLE_ACTIVE = 1;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'corplbr.comments';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id_user', 'fk_id', 'type_fk', 'title', 'content', 'dates', 'visible', 'admin_accept'], 'required'],
+            [['id_user', 'fk_id', 'visible', 'admin_accept', 'id_comment_to'], 'integer'],
+            [['type_fk', 'title', 'content', 'dates'], 'string'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'id_user' => 'Id User',
+            'fk_id' => 'Fk ID',
+            'type_fk' => 'Type Fk',
+            'title' => 'Title',
+            'content' => 'Content',
+            'dates' => 'Dates',
+            'visible' => 'Visible',
+            'admin_accept' => 'Admin Accept',
+            'id_comment_to' => 'Id Comment To',
+        ];
+    }
+
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'id_user']);
+    }
+
+    public function getComment()
+    {
+        return $this->hasOne(Comments::className(), ['id' => 'id_comment_to']);
+    }
+
+    public function getSubComments()
+    {
+        return $this->hasMany(Comments::className(), ['id_comment_to' => 'id']);
+    }
+}
