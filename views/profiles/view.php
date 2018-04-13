65e566392b69cb6d0a1e03b741ed8045799c9b88
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-04-13 16:31:52 +0300

    editProfiles

--- views/profiles/view.php
+++ views/profiles/view.php
@@ -6,17 +6,26 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
 
-$this->title = "Профиль: " . $model->id;
+$this->title = "Профиль: " . $model->first_name . " " . $model->last_name;
 $this->params['breadcrumbs'][] = ['label' => 'Сотрудники', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="user-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <?
-    echo "<pre>".print_r($model->first_name, true)."</pre>";
-    echo "<pre>".print_r($model->img, true)."</pre>";
-    echo "<pre>".print_r($model->position, true)."</pre>";
-    ?>
+<div class="row">
+	<div class="col-xs-12 col-md-4">
+        <div class="card m-b-30" style="padding: 20px;">
+            <img class="card-img-top img-fluid" src="http://portal.lbr.ru/img/user/thumbnail_<?=$model->img?>" alt="Card image cap" style = "border-radius: 5px">
+            <div class="card-body" style="padding: 10px 0px">
+                <h5 class="card-title" style="font-weight: bold;color: black"><?=$model->first_name?> <?=$model->last_name?></h5>
+                <p class="card-text">
+                    <?=$model->department?> <br>
+                    <?=$model->position?> <br>
+                </p>
+                <p class="card-text">
+                    <small class="text-muted">Был в сети 3 минуты назад</small>
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
