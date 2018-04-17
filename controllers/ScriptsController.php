d091622f2aa43401d26b87210711e284545115fc
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-04-17 19:32:12 +0300

    editScripts

--- controllers/ScriptsController.php
+++ controllers/ScriptsController.php
@@ -62,14 +62,18 @@ class ScriptsController extends Controller
         $scripts = Scripts::find()->orderBy('id asc')->where(['id_fk_scripts' => null])->all();
         return $this->render('index', compact("scripts"));
     }
-    public function actionUpdate($id) {
-        if ($id == 'reset') {
+    public function actionUpdate($id, $reset = null) {
+        if ($reset) {
             $reset=true;
             $scripts = Scripts::find()->orderBy('id asc')->where(['id_fk_scripts' => null])->all();
         } else {
             $data = Scripts::find()->where(['id' => $id])->one();
             $answers = Scripts::find()->orderBy('id desc')->where(['id_fk_scripts' => $id])->all();
+            if (!$answers and $data['redir']) {
+                $data = Scripts::find()->where(['id' => $data['redir']])->one();
+            }
         }
+
         
         return $this->renderPartial('view', compact("data", "answers", "reset", "scripts"));
     }
--- views/scripts/index.php
+++ views/scripts/index.php
@@ -4,18 +4,24 @@
 
 use yii\helpers\Html;
 
+
 $this->title = 'Скрипты';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 
+
 <?php //  echo var_dump($scripts) ?>
 
-<div class="row" >
-	<div class="col-xs-12 col-xl-8 card-box container-fluid" id = "scripts">
-		<?php foreach ($scripts as $script): ?>
-			<a href="#src" onclick = "ajax('update', 'id=<?=$script['id']?>', 'scripts')"><?=$script['answer']?></a> <br>
-		<?php endforeach ?>
+<div class="row hidden-xs" style="color: black!important;" >
+	<div class="col-12 card-box" id = "scripts">
+		<div class="row">
+			<div class="col-xl-4">
+				<?php foreach ($scripts as $script): ?>
+					<button class = "btn btn-light waves-effect waves-light btn-block" href="#src" onclick = "ajax('update', 'id=<?=$script['id']?>', 'scripts')" style = "text-align: left;"><i class="mdi mdi-arrow-right-bold-hexagon-outline"></i> <?=$script['answer']?></button>
+				<?php endforeach ?>
+			</div>
+		</div>
 	</div>
 </div> 
 
