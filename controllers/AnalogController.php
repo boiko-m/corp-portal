563c3341f321e6e09170dffb84f718dc0d111cd3
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-04-27 15:06:57 +0300

    editAnalog

--- controllers/AnalogController.php
+++ controllers/AnalogController.php
@@ -8,7 +8,7 @@ class AnalogController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-
+    	$this->layout = false;
         return $this->renderPartial('index');
     }
     
