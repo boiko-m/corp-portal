f7a7d1703b865def8f1a5cb314635ca64ce02136
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-04-27 15:46:38 +0300

    editAnalog

--- controllers/AnalogController.php
+++ controllers/AnalogController.php
@@ -9,6 +9,8 @@ class AnalogController extends \yii\web\Controller
     public function actionIndex($json = null)
     {
     	$this->layout = false;
+    	$this->enableCsrfValidation = false;
+    	
         return $this->renderPartial('index');
     }
     
