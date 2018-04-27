3ae294f496ce1f9b54e8cd85d10a36231e6a52bf
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-04-27 16:00:36 +0300

    editAnalog

--- controllers/AnalogController.php
+++ controllers/AnalogController.php
@@ -9,10 +9,15 @@ class AnalogController extends \yii\web\Controller
     public function actionIndex($json = null)
     {
     	$this->layout = false;
-    	$this->enableCsrfValidation = false;
-    	
+
         return $this->renderPartial('index');
     }
+
+    public function beforeAction($action) {
+        $this->enableCsrfValidation = ($action->id !== "index"); 
+        return parent::beforeAction($action);
+    }
+
     
 
 }
