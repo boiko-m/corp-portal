90c39473e7dafe2dbab7dc639f9710f0fc1e4bc8
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-05-22 15:28:17 +0300

    editKnowledge

--- views/knowledge/indebtedness.php
+++ views/knowledge/indebtedness.php
@@ -12,7 +12,10 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="card-box">
             <div class="row">
                 <div class="col-12">
-                   <iframe src="http://portal.lbr.ru/video_unload/effect_client1.mp4" frameborder="0" style="width: 100%;height: 500px;border:1px solid #dfdfdf"></iframe>
+                	<video width="100%" controls>
+					  <source src="/video/effect_client1.mp4" type="video/mp4">
+					  Your browser does not support the video tag.
+					</video>
                 </div>
             </div>
         </div>
