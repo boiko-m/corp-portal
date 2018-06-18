d1877b6156b82637fbddb7fd7c5464a7bef8fdb8
okotchik <okotchik@lbr.ru>
2018-06-18 08:34:04 +0300

    123

--- views/profiles/view.php
+++ views/profiles/view.php
@@ -28,12 +28,8 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="row">
                 <div class="col-9">
                     <div >
-                        <h5  class="card-title" style="font-weight: bold;color: black"><?=$model->last_name?> <?=$model->first_name?> <?=$model->middle_name?>
-                            <?php
-                            if($model->id == Yii::$app->user->id){
-                                 echo Html::a('Редактировать профиль', '/profiles/update/'.$model->id);
-                            }
-                            ?></h5>
+                        <h5  class="card-title" style="font-weight: bold;color: black"></h5><?=$model->last_name?> <?=$model->first_name?> <?=$model->middle_name?>
+                           
 
 
                     </div>
