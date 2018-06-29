baee4af38b6ce901953b53200e64e51f5c97d799
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-06-29 14:34:35 +0300

    editIndexFile

--- web/index.php
+++ web/index.php
@@ -3,8 +3,10 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../vendor/autoload.php';
+date_default_timezone_set('Europe/Minsk');
+
+
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../functions.php';
 $config = require __DIR__ . '/../config/web.php';
