0e324178c46189e52c26afba669ce17955abf2e5
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-07-03 09:52:13 +0300

    fix tabulation

--- controllers/AnalogController.php
+++ controllers/AnalogController.php
@@ -8,16 +8,15 @@ class AnalogController extends \yii\web\Controller
 {
     public function actionIndex($json = null)
     {
-    	$this->layout = false;
-
+    	  $this->layout = false;
         return $this->renderPartial('index');
     }
 
     public function beforeAction($action) {
-        $this->enableCsrfValidation = ($action->id !== "index"); 
+        $this->enableCsrfValidation = ($action->id !== "index");
         return parent::beforeAction($action);
     }
 
-    
+
 
 }
