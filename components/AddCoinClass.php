13c55217284a64e2fe5bb67c0609f4f8f43bb421
Лешик Максим Игоревич <leshik@lbr.ru>
2018-07-04 15:23:22 +0300

    coins error fix

--- components/AddCoinClass.php
+++ components/AddCoinClass.php
@@ -6,17 +6,19 @@ class AddCoinClass  extends \yii\base\Component
 {
     public function init() {
 
-        $user = Profile::find()->where(['id' => Yii::$app->user->id])->one();
-        if( $user->last_coin == null){
-            $user->last_coin = time();
-            $user->coins = $user->coins + 6;
-            $user->save();
+        if($user = Profile::find()->where(['id' => Yii::$app->user->id])->one()) {
+            if( $user->last_coin == null){
+                $user->last_coin = time();
+                $user->coins = $user->coins + 6;
+                $user->save();
+            }
+            $currentData = date('y,m,d', time());
+            if($currentData != date('y,m,d',$user->last_coin)){
+                $user->coins = $user->coins + 3;
+                $user->last_coin = time();
+                $user->save();
+            }
         }
-        $currentData = date('y,m,d', time());
-        if($currentData != date('y,m,d',$user->last_coin)){
-            $user->coins = $user->coins + 3;
-            $user->last_coin = time();
-            $user->save();
-        }
+
     }
 }
