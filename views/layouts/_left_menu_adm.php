3b962a4fda38d20e40bd41719210b2033fe231a3
Гавриленко Андрей Сергеевич <gavrilenko@lbr.ru>
2018-08-02 13:38:35 +0300

    editAdminPanelLeft

--- views/layouts/_left_menu_adm.php
+++ views/layouts/_left_menu_adm.php
@@ -25,95 +25,99 @@ use app\models\WantToProject;
             <!-- Left Menu Start -->
             <ul class="metismenu" id="side-menu">
                 <li class="menu-title">Админ-панель</li>
-                <li>
-                    <a href="/admin/">
-                        <i class="fi-grid"></i> <span> Главная </span>
-                    </a>
-                </li>
-                <?if(\Yii::$app->user->can("allAccess")):?>
-                <li>
-                    <a href="javascript: void(0);"><i class="fi-file"></i><span> Роли </span> <span class="menu-arrow"></span></a>
-                    <ul class="nav-second-level" aria-expanded="false">
-                        <li><a href="/rbac/route">Маршруты</a></li>
-                        <li><a href="/rbac/permission">Разрешения</a></li>
-                        <li><a href="/rbac/role">Роли</a></li>
-                        <li><a href="/rbac/assignment">Назначения</a></li>
-                    </ul>
-                </li>
-                <?endif;?>
-                <?if(\Yii::$app->user->can("allAccess")):?>
-                <li>
-                    <a href="javascript: void(0);"><i class="fi-file"></i><span> Пользователи </span> <span class="menu-arrow"></span></a>
-                    <ul class="nav-second-level" aria-expanded="false">
-                        <li><a href="/admin/userup/">Обновление</a></li>
-                        <li><a href="/admin/user">Дан. авторизации</a></li>
-                        <li><a href="/admin/profile">Профили</a></li>
-                        <li><a href="/admin/setting-options">Настройки интерфейса</a></li>
-                    </ul>
-                </li>
-                <?endif;?>
-                <?if(\Yii::$app->user->can("controlProject")):?>
-                <li>
-                    <a href="javascript: void(0);"><i class="fi-file"></i><span> Проекты </span> <span class="menu-arrow"></span></a>
-                    <ul class="nav-second-level" aria-expanded="false">
-                        <li><a href="/admin/projects">Проекты</a></li>
-                        <li><a href="/admin/project-news">Новости проектов</a></li>
-                        <li><a href="/admin/project-user">Назначения</a></li>
-                        <li><a href="/admin/want-to-project">Желающие<span class="badge badge-info badge-info-status"><?= $unconfirmedStatements ?></span></a></li>
-                        <li><a href="/admin/project-user-group">Группы пользователей</a></li>
-                    </ul>
-                </li>
-                <?endif;?>
-                <?if(\Yii::$app->user->can("controlMessages")):?>
-                <li>
+                <div>
+                    <div>
+                        <li>
+                            <a href="/admin/">
+                                <i class="fi-grid"></i> <span> Главная </span>
+                            </a>
+                        </li>
+                        <?if(\Yii::$app->user->can("allAccess")):?>
+                        <li>
+                            <a href="javascript: void(0);"><i class="fi-file"></i><span> Роли </span> <span class="menu-arrow"></span></a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="/rbac/route">Маршруты</a></li>
+                                <li><a href="/rbac/permission">Разрешения</a></li>
+                                <li><a href="/rbac/role">Роли</a></li>
+                                <li><a href="/rbac/assignment">Назначения</a></li>
+                            </ul>
+                        </li>
+                        <?endif;?>
+                        <?if(\Yii::$app->user->can("allAccess")):?>
+                        <li>
+                            <a href="javascript: void(0);"><i class="fi-file"></i><span> Пользователи </span> <span class="menu-arrow"></span></a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="/admin/userup/">Обновление</a></li>
+                                <li><a href="/admin/user">Дан. авторизации</a></li>
+                                <li><a href="/admin/profile">Профили</a></li>
+                                <li><a href="/admin/setting-options">Настройки интерфейса</a></li>
+                            </ul>
+                        </li>
+                        <?endif;?>
+                        <?if(\Yii::$app->user->can("controlProject")):?>
+                        <li>
+                            <a href="javascript: void(0);"><i class="fi-file"></i><span> Проекты </span> <span class="menu-arrow"></span></a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="/admin/projects">Проекты</a></li>
+                                <li><a href="/admin/project-news">Новости проектов</a></li>
+                                <li><a href="/admin/project-user">Назначения</a></li>
+                                <li><a href="/admin/want-to-project">Желающие<span class="badge badge-info badge-info-status"><?= $unconfirmedStatements ?></span></a></li>
+                                <li><a href="/admin/project-user-group">Группы пользователей</a></li>
+                            </ul>
+                        </li>
+                        <?endif;?>
+                        <?if(\Yii::$app->user->can("controlMessages")):?>
+                        <li>
 
-                    <a href="javascript: void(0);"><i class="fi-file"></i><span> Сообщения </span> <span class="menu-arrow"></span></a>
-                    <ul class="nav-second-level" aria-expanded="false">
-                        <li><a href="/admin/im-groups/">Группы</a></li>
-                        <li><a href="/admin/type-group-im">Тип групп</a></li>
-                        <li><a href="/admin/attachments-message">Вложения сообщений</a></li>
-                    </ul>
+                            <a href="javascript: void(0);"><i class="fi-file"></i><span> Сообщения </span> <span class="menu-arrow"></span></a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="/admin/im-groups/">Группы</a></li>
+                                <li><a href="/admin/type-group-im">Тип групп</a></li>
+                                <li><a href="/admin/attachments-message">Вложения сообщений</a></li>
+                            </ul>
 
-                </li>
-                <?endif;?>
-                <?if(\Yii::$app->user->can("controlNews")):?>
-                <li>
-                    <a href="/admin/news">
-                        <i class="fi-file"></i> <span>Новости</span><span class="badge badge-info badge-info-status"><?= $unconfirmedNews ?></span>
-                    </a>
-                </li>
-                <?endif;?>
-                <?if(\Yii::$app->user->can("controlVideo")):?>
-                <li>
-                    <a href="/admin/videos">
-                        <i class="fi-file"></i> <span>Видео</span>
-                    </a>
-                </li>
-                <?endif;?>
-                <?if(\Yii::$app->user->can("controlFAQ")):?>
-                <li>
-                    <a href="javascript: void(0);"><i class="fi-file"></i><span> Обуч. материал </span> <span class="menu-arrow"></span></a>
-                    <ul class="nav-second-level" aria-expanded="false">
-                        <li><a href="/admin/faq">FAQ</a></li>
-                        <li><a href="/admin/faq-type">Типы</a></li>
-                    </ul>
-                </li>
-                <?endif;?>
-                <?if(\Yii::$app->user->can("controlGift")):?>
-                <li>
-                    <a href="javascript: void(0);"><i class="fi-file"></i><span> Подарки </span> <span class="menu-arrow"></span></a>
-                    <ul class="nav-second-level" aria-expanded="false">
-                        <li><a href="/admin/gift">Подарки</a></li>
-                        <li><a href="/admin/gift-type">Типы подарков</a></li>
-                    </ul>
-                </li>
-                <?endif;?>
+                        </li>
+                        <?endif;?>
+                        <?if(\Yii::$app->user->can("controlNews")):?>
+                        <li>
+                            <a href="/admin/news">
+                                <i class="fi-file"></i> <span>Новости</span><span class="badge badge-info badge-info-status"><?= $unconfirmedNews ?></span>
+                            </a>
+                        </li>
+                        <?endif;?>
+                        <?if(\Yii::$app->user->can("controlVideo")):?>
+                        <li>
+                            <a href="/admin/videos">
+                                <i class="fi-file"></i> <span>Видео</span>
+                            </a>
+                        </li>
+                        <?endif;?>
+                        <?if(\Yii::$app->user->can("controlFAQ")):?>
+                        <li>
+                            <a href="javascript: void(0);"><i class="fi-file"></i><span> Обуч. материал </span> <span class="menu-arrow"></span></a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="/admin/faq">FAQ</a></li>
+                                <li><a href="/admin/faq-type">Типы</a></li>
+                            </ul>
+                        </li>
+                        <?endif;?>
+                        <?if(\Yii::$app->user->can("controlGift")):?>
+                        <li>
+                            <a href="javascript: void(0);"><i class="fi-file"></i><span> Подарки </span> <span class="menu-arrow"></span></a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="/admin/gift">Подарки</a></li>
+                                <li><a href="/admin/gift-type">Типы подарков</a></li>
+                            </ul>
+                        </li>
+                        <?endif;?>
 
-                <?if(\Yii::$app->user->can("controlBroadcast")):?>
-                <li>
-                    <a href="/admin/broadcast"><i class="fi-file"></i><span> Трансляции </span></a>
-                </li>
-                <?endif;?>
+                        <?if(\Yii::$app->user->can("controlBroadcast")):?>
+                        <li>
+                            <a href="/admin/broadcast"><i class="fi-file"></i><span> Трансляции </span></a>
+                        </li>
+                        <?endif;?>
+                    </div>
+                </div>
 
             </ul>
 
