e51605dfb5a1fe96ef66dfbcda1dc6c258cece08
SteepBeaver <davidyan@lbr.ru>
2018-08-28 14:35:52 +0300

    Second intermediate commit because of necessary doing edits projects info

--- views/project/view.php
+++ views/project/view.php
@@ -16,33 +16,39 @@
 ?>
 
 <div class="row">
-  <div class="col-xs-9 col-md-9 ">
+  <div class="col-xs-9 col-md-9">
     <div class="card">
-      <ul class="nav nav-tabs nav-justified nav-project " style="margin: 10px;">
+      <ul class="nav nav-tabs nav-justified nav-project" style="margin: 10px;">
         <li class="nav-item">
-          <a href="#spr1" class="nav-link active" title="Дата проведения: 01.01.2018 - 21.02.2018" data-toggle="tab" aria-expanded="false" onclick="tajax('/project/all', {
-            container : 'projectall',
-            data: 'id=1'
-          })">1 этап</a>
+            <a href="#spr1" class="nav-link active" title="Дата проведения: 01.01.2018 - 21.02.2018" data-toggle="tab" aria-expanded="false" onclick="tajax('/project/all', {
+              container : 'projectall',
+              data: 'id=1'
+            })">1 этап
+            <? if (Yii::$app->user->can('Scrum-master')) : ?>
+              <i class="fa fa-minus delete-element" aria-hidden="true" title="Удалить этап" onclick="alert('Удалить этап');" style="float: right"></i>
+            <? endif; ?>
+          </a>
         </li>
         <li class="nav-item col-xs-3 col-md-3">
-          <a href="#target2" class="nav-link" data-toggle="tab" aria-expanded="false" onclick="tajax('/project/infoajax', {
+          <a href="#target1" class="nav-link" data-toggle="tab" aria-expanded="false" onclick="tajax('/project/infoajax', {
             container : 'projectall',
             data: 'id=1'
-          })">Проект <br> <br></a>
+          })">Проект</a>
         </li>
+        <? if (Yii::$app->user->can('Scrum-master')) : ?>
+          <li class="nav-item col-xs-3 col-md-3">
+            <a href="#" class="nav-link" title="Добавить этап" onclick="alert('Добавить этап');"><i class="fa fa-plus" aria-hidden="true"></i></a>
+          </li>
+        <? endif; ?>
       </ul>
 
       <div>
-          <div id="projectall">
-              // контент
-          </div>
+          <div id="projectall"></div>
       </div>
-
     </div>
-
   </div>
-  <div class="col-xs-12 col-md-3 ">
+
+  <div class="col-xs-12 col-md-3 right-menu-project">
     <div class="card">
       <ul class="nav nav-tabs tabs-bordered nav-justified nav-project ">
         <!-- <li class="nav-item col-xs-12">
@@ -56,7 +62,7 @@
         </li> -->
       </ul>
 
-      <div style="padding: 0px 10px; padding-bottom: 10px" class="tab-content">
+      <div class="tab-content" style="padding: 0px 10px; padding-bottom: 10px">
         <!-- <div id="right1" class="tab-pane show">
 
           <div class="dropdown mt-3">
@@ -136,19 +142,21 @@
           <? foreach ($project_group as $key => $group) { ?>
             <div class="work-group-view">
               <?php if ($userGroup != $group->id_project_user_group): ?>
-                <div class="work-group-view-title" style="padding-top: 10px;">
+                <div class="work-group-view-title">
                   <?= (ProjectUserGroup::find()->where(['id' => $group->id_project_user_group])->one())->name ?>
                 </div>
               <? endif; ?>
               <div class="work-group-view-content">
                 <div class="btn-group m-b-10">
-                  <button type="button" class="btn btn-light"><?= $group['profile']['first_name'] . ' ' . $group['profile']['last_name'] ?></button>
-                  <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-                  <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(95px, 35px, 0px); top: 0px; left: 0px; will-change: transform;">
-                    <a class="dropdown-item" href="#">Добавить задачу</a>
-                    <div class="dropdown-divider"></div>
-                    <small style="padding: 10px;"><?= date('d.m.Y', $group->create_at) ?></small>
-                  </div>
+                  <button type="button" class="btn btn-light" onclick="window.open('/profiles/<?= $group['profile']['id'] ?>', '');"><?= $group['profile']['first_name'] . ' ' . $group['profile']['last_name'] ?></button>
+                  <? if (Yii::$app->user->can('Scrum-master')): ?>
+                    <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+                    <div class="dropdown-menu text-center" x-placement="bottom-start" style="position: absolute; transform: translate3d(95px, 35px, 0px); top: 0px; left: 0px; will-change: transform;">
+                      <a class="dropdown-item" href="#">Добавить задачу</a>
+                      <div class="dropdown-divider"></div>
+                      <small>Принят: <?= date('d.m.Y', $group->create_at) ?></small>
+                    </div>
+                  <? endif; ?>
                 </div>
               </div>
             </div>
--- views/project/view_bottom.php
+++ views/project/view_bottom.php
@@ -1,189 +1,146 @@
 ﻿<div class="row">
-    <div class="col-xs-12 col-md-12 ">
-        <div class="">
-            <h5  style="padding-left: 10px;">
-                Этап 3 <small> с 20.01.2018 по 20.02.2018</small>
-            </h5>
+  <div class="col-xs-12 col-md-12">
+    <h5 style="padding-left: 10px;">
+      <span style="font-size: 16px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt earum, reprehenderit sequi qui recusandae rem labore numquam, deserunt soluta quo?</span>
+    </h5>
 
-            <ul class="nav nav-tabs nav-justified nav-project ">
-                <li class="nav-item">
-                    <a href="#spr1" class="nav-link" data-toggle="tab" aria-expanded="false">Спринт<br>1</a>
-                </li>
-               <li class="nav-item">
-                    <a href="#spr2" class="nav-link active" data-toggle="tab" aria-expanded="false">Спринт 2<br><small>(20.02 - 20.03)</small> </a>
-                </li>
-                 <li class="nav-item">
-                    <a href="#spr3" class="nav-link" data-toggle="tab" aria-expanded="false">Спринт<br>3</a>
-                </li>
-                 <li class="nav-item">
-                    <a href="#spr4" class="nav-link" data-toggle="tab" aria-expanded="false">Спринт<br>4</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#spr4" class="nav-link" data-toggle="tab" aria-expanded="false">Спринт<br>5</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#target" class="nav-link" data-toggle="tab" aria-expanded="false">Цели и <br>итоги</a>
-                </li>
-            </ul>
+    <ul class="nav nav-tabs nav-justified nav-project" style="margin: 20px;">
+      <li class="nav-item">
+        <a href="#spr1" class="nav-link active" title="Дата проведения: 20.02.2018 - 20.03.2018" data-toggle="tab" aria-expanded="false">Спринт 1
+          <? if (Yii::$app->user->can('Scrum-master')): ?>
+            <i class="fa fa-minus delete-element" aria-hidden="true" title="Удалить спринт" onclick="alert('Удалить спринт');" style="float: right;"></i>
+          <? endif; ?>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#results" class="nav-link" data-toggle="tab" aria-expanded="false">Цели и итоги</a>
+      </li>
+      <? if (Yii::$app->user->can('Scrum-master')): ?>
+        <li class="nav-item">
+          <a href="#" class="nav-link" title="Добавить спринт" onclick="alert('Добавить спринт');"><i class="fa fa-plus" aria-hidden="true"></i></a>
+        </li>
+      <? endif; ?>
+    </ul>
 
+    <div class="tab-content" style="padding: 20px;">
+      <div id="spr1" class="tab-pane show active">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt earum, reprehenderit sequi qui recusandae rem labore numquam, deserunt soluta quo?</p>
+        <table class="table project-table table-striped">
+          <thead>
+            <th>Задача</th>
+            <th style="text-align: center;">План.</th>
+            <th>Факт.</th>
+            <th>%</th>
+            <th>Исполнитель</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <a href=""><i class="dripicons-document-edit"></i></a>
+                <small><a href="" class="card-link">Разобраться, как конкуренты предлагают зпч, сразу с доставкой или после</a></small>
+              </td>
+              <td>3</td>
+              <td>1</td>
+              <td>33</td>
+              <td><a href="" class="card-link">Гавриленко А.С.</a></td>
+            </tr>
+            <tr>
+              <td>
+                <small><a href="" class="card-link">Доделать алгоритм автоматизации доставки в 1С и сделать ТЗ в ИТ </a></small>
+              </td>
+              <td>3</td>
+              <td>32</td>
+              <td>5</td>
+              <td><a href="" class="card-link">Савченко А.Е.</a></td>
+            </tr>
+            <tr>
+              <td>
+                <small><a href="" class="card-link">Сделать в алгоритме автоматизации в блоке транспорта предложения, замечания </a></small>
+              </td>
+              <td>3</td>
+              <td>3</td>
+              <td>100</td>
+              <td><a href="" class="card-link">Михлай И.К.</a></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-                <div  class="tab-content">
-                    <div id="spr1" class="tab-pane  show ">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est excepturi quis totam expedita voluptas veniam, molestias itaque perspiciatis libero aliquid, laborum possimus velit, nemo tempore in repudiandae beatae reprehenderit repellendus eius cumque enim numquam dolor. Eaque maiores fuga iure quia, necessitatibus, nesciunt architecto dolor magni. Voluptatum cum quae, vero laudantium!
-                        </p>
-                    </div>
-                    <div id="spr2" class="tab-pane show active">
-                        <table class="table project-table table-striped">
-                            <thead>
-                                <th>Задача</th>
-                                <th style="text-align: center;">План.</th>
-                                <th>Факт.</th>
-                                <th>%</th>
-                                <th>Исполнитель</th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <a href=""><i class="dripicons-document-edit"></i></a>
-                                        <small><a href="" class="card-link">Разобраться, как конкуренты предлагают зпч, сразу с доставкой или после</a></small>
-                                    </td>
-                                    <td>3</td>
-                                    <td>1</td>
-                                    <td>33</td>
-                                    <td><a href="" class="card-link">Гавриленко А.С.</a></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <small><a href="" class="card-link">Доделать алгоритм автоматизации доставки в 1С и сделать ТЗ в ИТ </a></small>
-                                    </td>
-                                    <td>3</td>
-                                    <td>32</td>
-                                    <td>5</td>
-                                    <td><a href="" class="card-link">Савченко А.Е.</a></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <small><a href="" class="card-link">Сделать в алгоритме автоматизации в блоке транспорта предложения, замечания </a></small>
-                                    </td>
-                                    <td>3</td>
-                                    <td>3</td>
-                                    <td>100</td>
-                                    <td><a href="" class="card-link">Михлай И.К.</a></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
+      <div id="results" class="tab-pane show" style="padding: 0px 20px;">
+        <h5>Цели этапа </h5>
+          <div class="project-target" style="padding-left: 20px;">
+            <div class="project-target-items" style="background: #f5f5f5">
+              <div class="project-target-item">
+                Составить индивидуальный план рекламы (ИПР)  для  30 клиентов.Выбираем 3-4 филиала, на них взять по несколько клиентов, на ком тренируемся составлять ИПР - ставим по клиенту товарные цели на базе потенциала, чем занимается клиент. Определяем, что можем предложить, в какой срок, по каким каналам связи
+              </div>
+              <div style="padding-left: 20px;">
+                <small> Цель скорректирована:
+                  <b>Причина:</b> Перенос на следующий этап.
+                </small>
+              </div>
+              <div class="text-right">
+                  <small>Решено 5/30</small>
+              </div>
+              <div class="progress" style="height: 5px; margin: 0px; border: 1px solid #3ec39685;">
+                <div class="progress-bar progress-lg bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
 
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div id="spr3" class="tab-pane show ">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt earum, reprehenderit sequi qui recusandae rem labore numquam, deserunt soluta quo?
-                        </p>
-                    </div>
+            <div class="project-target-items">
+              <div class="project-target-item">
+                Посадочная страница для клиента - персонализировать данные для клиента. При переходе по ссылке клиент попадает на страницу с предложениями специально для него
+              </div>
+              <div style="padding-left: 20px;">
+                <small> Цель скорректирована:
+                  <b>Причина:</b> Перенос на следующий этап.
+                </small>
+              </div>
+            </div>
 
-                    <div id="spr4" class="tab-pane show ">
-                        <p>
-                            spr4
-                        </p>
-                    </div>
+            <div class="project-target-items success-border">
+              <div class="project-target-item">
+                Сформировать стандартный перечень должностей и их роли.
+              </div>
+              <div class="text-right">
+                <small>Цель достигнута!</small>
+              </div>
+            </div>
 
-                    <div id="spr5" class="tab-pane show ">
-                        <p>
-                            spr5
-                        </p>
-                    </div>
+            <div class="project-target-items success-border">
+              <div class="project-target-item">
+                Привести в порядок моб телефоны, емэйлы, найти контакты соц сетей.
+              </div>
+              <div class="text-right">
+                <small>Цель достигнута!</small>
+              </div>
+            </div>
+          </div>
 
-                    <div id="target" class="tab-pane show" style="padding: 0px 20px;">
-                        <h5>
-                            Цели этапа
-                        </h5>
-                        <div class="project-target" style="padding-left: 20px;">
-                            <div class = "project-target-items" style = "background: #f5f5f5">
-                                <div class="project-target-item">
-                                    Составить индивидуальный план рекламы (ИПР)  для  30 клиентов.Выбираем 3-4 филиала, на них взять по несколько клиентов, на ком тренируемся составлять ИПР - ставим по клиенту товарные цели на базе потенциала, чем занимается клиент. Определяем, что можем предложить, в какой срок, по каким каналам связи
-                                </div>
-                                <div style="padding-left: 20px;">
-                                    <small> Цель скорректирована:
-                                    <b>Причина:</b> Перенос на следующий этап.
-                                    </small>
-                                </div>
-                                <div class="text-right">
-                                    <small>Решено 5/30</small>
-                                </div>
+          <br><br>
+          <h5>Итоги этапа</h5>
 
-
-                                <div class="progress" style="height: 5px;margin:0px;border:1px solid #3ec39685;">
-                                    <div class="progress-bar progress-lg bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width:25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-
-                            </div>
-
-
-
-                            <div class = "project-target-items">
-                                <div class="project-target-item">
-                                    Посадочная страница для клиента - персонализировать данные для клиента. При переходе по ссылке клиент попадает на страницу с предложениями специально для него
-                                </div>
-                                <div style="padding-left: 20px;">
-                                    <small> Цель скорректирована:
-                                    <b>Причина:</b> Перенос на следующий этап.
-                                    </small>
-                                </div>
-                            </div>
-
-                            <div class = "project-target-items success-border">
-                                <div class="project-target-item">
-                                    Сформировать стандартный перечень должностей и их роли.
-                                </div>
-                                <div class="text-right">
-                                    <small>Цель достигнута!</small>
-                                </div>
-                            </div>
-
-                            <div class = "project-target-items success-border">
-                                <div class="project-target-item">
-                                    Привести в порядок моб телефоны, емэйлы, найти контакты соц сетей.
-                                </div>
-                                <div class="text-right">
-                                    <small>Цель достигнута!</small>
-                                </div>
-                            </div>
-
-
-                        </div>
-
-                        <br> <br>
-                        <h5>
-                            Итоги этапа
-                        </h5>
-                        <div class="project-target" style="padding-left: 20px;">
-                            <div class = "project-target-items">
-                                <div class="project-target-item">
-                                    Разобрались с  проблемой и выработали план действий на примере 3-х клиентов
-                                </div>
-                            </div>
-                            <div class = "project-target-items">
-                                <div class="project-target-item">
-                                    Выполнили задачи этапа, приняли решение закрыть этап с оценкой Хорошо (зеленая зона).
-                                </div>
-                                <small></small>
-                            </div>
-                            <br>
-                        </div>
-
-
-                    </div>
-
-                </div>
+          <div class="project-target" style="padding-left: 20px;">
+            <div class="project-target-items">
+              <div class="project-target-item">
+                Разобрались с проблемой и выработали план действий на примере 3-х клиентов
+              </div>
+            </div>
+            <div class="project-target-items">
+              <div class="project-target-item">
+                Выполнили задачи этапа, приняли решение закрыть этап с оценкой Хорошо (зеленая зона).
+              </div>
+            </div>
+            <br>
+          </div>
 
         </div>
-    </div>
-
+      </div>
+  </div>
 </div>
