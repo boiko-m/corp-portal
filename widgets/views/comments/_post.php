e4498b97d0888a09754e9a84a8a2da8169bd966d
Бойко Максим Александрович <boyko_m@lbr.ru>
2018-09-10 12:52:40 +0300

    foxed links in forum

--- widgets/views/comments/_post.php
+++ widgets/views/comments/_post.php
@@ -59,7 +59,7 @@ if ($model->model == 'project-news' && !$meta)
                 <small class="com_date"><?= date('d.m.y в H:m', $model->created_at) ?></small>
                 <span class="text-right small com_date">
                   <?php if ($model->model == 'project') echo ' | к проекту '; elseif ($model->model == 'project-news') echo ' | к новости '; ?>
-                  <a href="<?=$model->url?>"><?=$meta?></a>
+                  <a href="<?=$model->url?>" target="_blank"><?=$meta?></a>
                 </span>
             </h6>
             <?php if ($model->parent_id) : ?>
